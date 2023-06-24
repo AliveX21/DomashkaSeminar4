8e4dfb5312979daad3f1d0d965540af37547e381
unknown <Alive_1986@mail.ru>
2023-06-24 19:15:10 +0300

    Task2:Done

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (num != 0)
+{
+    int lustDigit = num % 10;
+    sum = sum + lustDigit;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма всех цифр в числе: {sum}");
